7e966657799b7178627b438080a4dbc9d10eabac
BorisKofman <Borisk@salt.security>
2024-07-04 13:22:11 +0300

    Update ESP32-AirConditioner.ino

--- ESP32-AirConditioner.ino
+++ ESP32-AirConditioner.ino
@@ -1,14 +1,18 @@
 #include "HomeSpan.h"
 #include <DHT.h>
+
+//For IR 
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 #include <IRrecv.h>
 #include <IRutils.h>
+
 #include <Preferences.h>  // For NVS
 
 #include <ir_Goodweather.h>
 
 #define DHT_PIN 21  // DHT11 sensor pin
+//define DHT_TYPE DHT22
 #define DHT_TYPE DHT11
 
 DHT dht(DHT_PIN, DHT_TYPE);
@@ -36,6 +40,7 @@ IRGoodweatherAc ac(kIrLedPin);
 
 Preferences preferences;
 
+
 class HeaterCooler : public Service::HeaterCooler {
   SpanCharacteristic *active;
   SpanCharacteristic *currentState;
@@ -56,12 +61,12 @@ public:
     active = new Characteristic::Active(0, true); // default to Off, stored in NVS
     currentState = new Characteristic::CurrentHeaterCoolerState(0, true); // Inactive, stored in NVS
     targetState = new Characteristic::TargetHeaterCoolerState(0, true); // Auto, stored in NVS
-    currentTemp = new Characteristic::CurrentTemperature();
+    currentTemp = new Characteristic::CurrentTemperature(0);
     coolingTemp = new Characteristic::CoolingThresholdTemperature(24.0, true); // default cooling temp, stored in NVS
     heatingTemp = new Characteristic::HeatingThresholdTemperature(27, true); // default heating temp, stored in NVS
     rotationSpeed = new Characteristic::RotationSpeed(50, true); // default fan speed, stored in NVS
     unit = new Characteristic::TemperatureDisplayUnits(0, true); // 0 for Celsius, stored in NVS
-    currentHumidity = new Characteristic::CurrentRelativeHumidity();
+    currentHumidity = new Characteristic::CurrentRelativeHumidity(0);
     swingMode = new Characteristic::SwingMode(0, true); // default to Swing Disabled, stored in NVS
 
     coolingTemp->setRange(16, 31, 1); // Set valid range for cooling temperature
@@ -80,15 +85,17 @@ public:
       active->setVal(ac.getPower());
       currentState->setVal(ac.getMode());
       coolingTemp->setVal(ac.getTemp());
+      
     }
   }
 
   void readTemperatureAndHumidity() {
     float temperature = dht.readTemperature();
-    float humidity = dht.readHumidity();
+    int humidity = dht.readHumidity();
 
     if (!isnan(temperature) && !isnan(humidity)) {
-      currentTemp->setVal(temperature);
+      int roundedTemp = round(temperature);
+      currentTemp->setVal(roundedTemp);
       currentHumidity->setVal(humidity);
     } else {
       Serial.println("Failed to read from DHT sensor!");
@@ -99,38 +106,41 @@ public:
     if (active->getNewVal() == 0) {
       // Turn Off the AC
       ac.setPower(false); // Send IR command to turn off the AC
+      irsend.sendGoodweather(ac.getRaw(), kGoodweatherBits);
+      return true;
     } else if (active->getNewVal() == 1) {
       // Turn On the AC
       ac.setPower(true); // Ensure the AC is on
 
       // Set fan speed based on rotationSpeed
       int speed = rotationSpeed->getNewVal();
-      if (speed <= 25) {
-          ac.setFan(kGoodweatherFanLow); // Set fan speed to low
-      } else if (speed <= 50) {
-          ac.setFan(kGoodweatherFanMed); // Set fan speed to medium
-      } else if (speed <= 75) {
-          ac.setFan(kGoodweatherFanHigh); // Set fan speed to high
-      } else {
-        ac.setFan(kGoodweatherFanAuto);
-      }
+      ac.setFan((speed <= 25) ? kGoodweatherFanLow :
+          (speed <= 50) ? kGoodweatherFanMed :
+          (speed <= 75) ? kGoodweatherFanHigh :
+                          kGoodweatherFanAuto);
+                          
       ac.setSwing(swingMode->getNewVal());  // Set swing mode
       int state = targetState->getNewVal();
       currentState->setVal(state == 0 ? 1 : (state == 1 ? 2 : 3)); // Set current state based on target state
-      if (state == 0) { // Auto
-        ac.setMode(kGoodweatherAuto);
-        ac.setTemp(coolingTemp->getNewVal());  // Set temperature
-      } else if (state == 1) { // Heating
-        ac.setTemp(heatingTemp->getNewVal());  // Set temperature
-        ac.setMode(kGoodweatherHeat);  //
-      } else if (state == 2) { // Cooling
-        ac.setMode(kGoodweatherCool);  // Set mode to cooling
-        ac.setTemp(coolingTemp->getNewVal());  // Set temperature
+      switch (state) {
+      case 0:
+          ac.setMode(kGoodweatherAuto);
+          ac.setTemp(coolingTemp->getNewVal());  // Set temperature
+          break;
+      case 1:
+          ac.setMode(kGoodweatherHeat);
+          ac.setTemp(heatingTemp->getNewVal());  // Set temperature
+          break;
+      case 2:
+          ac.setMode(kGoodweatherCool);
+          ac.setTemp(coolingTemp->getNewVal());  // Set temperature
+          break;
       }
-    }
+ 
     // Send IR command for cooling mode with specified settings
     irsend.sendGoodweather(ac.getRaw(), kGoodweatherBits);
     return true;
+    }
   } 
 };
 
@@ -143,33 +153,28 @@ void setup() {
   homeSpan.begin(Category::AirConditioners, "Air Conditioner");
   homeSpan.setApTimeout(180); // Set the timeout to 180 seconds (adjust as needed)
   homeSpan.enableAutoStartAP();
-  // homeSpan.setControlCallback(pairUnpairCallback); // Set the pairing/unpairing callback function future 
+  // homeSpan.setControlCallback(pairUnpairCallback); // Set the pairing/unpairing callback function in the future 
 
-  new SpanAccessory();
+new SpanAccessory();
   new Service::AccessoryInformation();
-  new Characteristic::Identify();
+    new Characteristic::Identify();
+    new Characteristic::Name("ESP32 Air Conditioner");
+    new Characteristic::Model("ESP32 AC Model");
+    new Characteristic::FirmwareRevision("1.0.1");
+
   new HeaterCooler();
-  new Characteristic::Name("ESP32 Air Conditioner");
-  new Characteristic::Manufacturer("ESP32");
-  new Characteristic::SerialNumber("123-ABC");
-  new Characteristic::Model("ESP32 AC Model");
-  new Characteristic::FirmwareRevision("1.1");
-  new Characteristic::Identify();
 }
 
 void loop() {
   // Check if the IR code has been received.
-  if (irrecv.decode(&results)) {
+if (irrecv.decode(&results)) {
     preferences.begin("ac_ctrl", false);  // Re-open NVS storage with namespace "esp32_air_conditioner"
     String irType = typeToString(results.decode_type);
-    if (!preferences.isKey("irType")) {
-      // Save the IR type to NVS
-      if (preferences.putString("irType", irType)) {
+    if (!preferences.isKey("irType") && preferences.putString("irType", irType)) {
         Serial.println(irType);
-      }
-      preferences.end();  // Close NVS storag
     }
-  }
+    preferences.end();  // Close NVS storage
+}
   homeSpan.poll();
 }
 
